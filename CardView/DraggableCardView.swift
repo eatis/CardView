fa32257c614fa9c88140a3ed641dbf939fc80fc9
hiroki.tanida <hiroki.tanida@leverages.jp>
2016-02-18 19:18:11 +0900

    infomation label の追加

--- CardView/DraggableCardView.swift
+++ CardView/DraggableCardView.swift
@@ -5,8 +5,16 @@
 
 import UIKit
 
+protocol DraggableCardViewDelegate {
+    func cardSwipedLeft(card: UIView)
+    func cardSwipedRight(card: UIView)
+}
+
 class DraggableCardView: UIView {
 
+    var delegate: DraggableCardViewDelegate?
+    var infomation: UILabel = UILabel()
+    
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -19,5 +27,20 @@ class DraggableCardView: UIView {
         return UINib(nibName: "CardView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! DraggableCardView
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+        infomation = UILabel(frame: CGRectMake(0, 50, self.frame.size.width, 100))
+        infomation.text = "no info given"
+        infomation.textAlignment = NSTextAlignment.Center
+        infomation.textColor = UIColor.blackColor()
+    }
+    
+    func setupView() {
+        // setup code
+    }
 }
